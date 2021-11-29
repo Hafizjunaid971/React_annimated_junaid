a3b3cc7e51ebe50737893cf755a281002090b1e6
junaid <hafizjunaid971@gmail.com>
2021-11-29 18:32:27 +0500

    ready to deploy

---
+++ src/data.js
@@ -0,0 +1,51 @@
+import ad1 from "../src/img/ubit AD2.jpg"
+import ad2 from "../src/img/award.png";
+import ad3 from "../src/img/ad3.jpg";
+import ad5 from "../src/img/ad5.jpg";
+import ad6 from "../src/img/ad6.jpg";
+import ad7 from "../src/img/ad7.jpg";
+import ad8 from "../src/img/ad8.jpg";
+import my from "../src/img/my.jpg";
+import hd1 from "../src/img/hd1.jpg";
+import hd2 from "../src/img/hd2.jpg";
+import fyp from "../src/img/fyp.png";
+export const products = [
+  {
+    id: 1,
+    img: hd1,
+    link: ad8,
+  
+  },
+  
+  {
+    id: 2,
+    // img: "",
+    img: ad1,
+    link: hd1,
+    
+  },
+  {
+    id: 3,
+    img: ad3,
+    link:ad5,
+
+  },
+  {
+    id: 4,
+    img: fyp,
+    link:ad6,
+   
+  },
+  {
+    id: 5,
+    img: hd2,
+    link:ad3,
+   
+  },
+  {
+    id: 6,
+    img: ad7,
+    link:my,
+ 
+  },
+];
